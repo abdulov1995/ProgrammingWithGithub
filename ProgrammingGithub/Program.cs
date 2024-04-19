ed464ae298f8337c9e4ac06599412dc2f8add48f
abdulov <kamal.abdulov.95@mail.ru>
2024-04-20 00:57:57 +0400

    Hello world

---
+++ ProgrammingGithub/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ProgrammingGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int password = 1234;
+            int maxAttempts = 3;
+
+            Console.WriteLine("Enter PIN: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (password != userInput)
+            {
+                for (int attempt = 1; attempt <= maxAttempts; attempt++)
+                {
+                    Console.WriteLine(maxAttempts - attempt + " chances left");
+                    Console.WriteLine("Enter PIN: ");
+                    int pin2 = Convert.ToInt32(Console.ReadLine());
+                    if (maxAttempts - attempt == 0)
+                    {
+                        Console.WriteLine("No more chances. Access denied!");
+                        Console.ReadLine();
+                        break;
+                    }
+
+
+
+                    if (pin2 == password)
+                    {
+                        Console.WriteLine("Correct PIN. Access granted!");
+                        return; 
+                    }
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Correct PIN. Access granted!");
+            }
+        }
+    }
+    
+}
